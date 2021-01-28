b1dde8b5dff7afc556d48869ccfc764442bf2257
Brijeshjha670 <brijeshjha18011997@gmail.com>
2021-01-28 07:54:22 -0800

    working on expanse and income displaying on ui

--- budgetapp.js
+++ budgetapp.js
@@ -1,247 +1,154 @@
-<!DOCTYPE html>
-
-<html lang="en">
-
-    <head>
-
-        <meta charset="UTF-8">
-
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans:100,300,400,600" rel="stylesheet" type="text/css">
-
-        <link href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet" type="text/css">
-
-        <link type="text/css" rel="stylesheet" href="budget.css">
-
-        <title>Budgety</title>
-
-    </head>
-
-    <body>
-
-        
-
-        <div class="top">
-
-            <div class="budget">
-
-                <div class="budget__title">
-
-                    Available Budget in <span class="budget__title--month">%Month%</span>:
-
-                </div>
-
-                
-
-                <div class="budget__value">+ 2,345.64</div>
-
-                
-
-                <div class="budget__income clearfix">
-
-                    <div class="budget__income--text">Income</div>
-
-                    <div class="right">
-
-                        <div class="budget__income--value">+ 4,300.00</div>
-
-                        <div class="budget__income--percentage">&nbsp;</div>
-
-                    </div>
-
-                </div>
-
-                
-
-                <div class="budget__expenses clearfix">
-
-                    <div class="budget__expenses--text">Expenses</div>
-
-                    <div class="right clearfix">
-
-                        <div class="budget__expenses--value">- 1,954.36</div>
-
-                        <div class="budget__expenses--percentage">45%</div>
-
-                    </div>
-
-                </div>
-
-            </div>
-
-        </div>
-
-        
-
-        
-
-        
-
-        <div class="bottom">
-
-            <div class="add">
-
-                <div class="add__container">
-
-                    <select class="add__type">
-
-                        <option value="inc" selected>+</option>
-
-                        <option value="exp">-</option>
-
-                    </select>
-
-                    <input type="text" class="add__description" placeholder="Add description">
-
-                    <input type="number" class="add__value" placeholder="Value">
-
-                    <button class="add__btn"><i class="ion-ios-checkmark-outline"></i></button>
-
-                </div>
-
-            </div>
-
-            
-
-            <div class="container clearfix">
-
-                <div class="income">
-
-                    <h2 class="icome__title">Income</h2>
-
-                    
-
-                    <div class="income__list">
-
-                       
-
-                        <!--
-
-                        <div class="item clearfix" id="income-0">
-
-                            <div class="item__description">Salary</div>
-
-                            <div class="right clearfix">
-
-                                <div class="item__value">+ 2,100.00</div>
-
-                                <div class="item__delete">
-
-                                    <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
-
-                                </div>
-
-                            </div>
-
-                        </div>
-
-                        
-
-                        <div class="item clearfix" id="income-1">
-
-                            <div class="item__description">Sold car</div>
-
-                            <div class="right clearfix">
-
-                                <div class="item__value">+ 1,500.00</div>
-
-                                <div class="item__delete">
-
-                                    <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
-
-                                </div>
-
-                            </div>
-
-                        </div>
-
-                        -->
-
-                        
-
-                    </div>
-
-                </div>
-
-                
-
-                
-
-                
-
-                <div class="expenses">
-
-                    <h2 class="expenses__title">Expenses</h2>
-
-                    
-
-                    <div class="expenses__list">
-
-                       
-
-                        <!--
-
-                        <div class="item clearfix" id="expense-0">
-
-                            <div class="item__description">Apartment rent</div>
-
-                            <div class="right clearfix">
-
-                                <div class="item__value">- 900.00</div>
-
-                                <div class="item__percentage">21%</div>
-
-                                <div class="item__delete">
-
-                                    <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
-
-                                </div>
-
-                            </div>
-
-                        </div>
-
-
-
-                        <div class="item clearfix" id="expense-1">
-
-                            <div class="item__description">Grocery shopping</div>
-
-                            <div class="right clearfix">
-
-                                <div class="item__value">- 435.28</div>
-
-                                <div class="item__percentage">10%</div>
-
-                                <div class="item__delete">
-
-                                    <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
-
-                                </div>
-
-                            </div>
-
-                        </div>
-
-                        -->
-
-                        
-
-                    </div>
-
-                </div>
-
-            </div>
-
-            
-
-            
-
-        </div>
-
-        
-
-        <script src="budgetapp.js"></script>
-
-    </body>
-
-</html>
+//BUDGET CONTROLLER
+var budgetController = (function () {
+  //Creating FUNCTION CONSTRUCTOR for the expanse and income .
+  var Expense = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var Income = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  //Data Structure where we store our data
+
+  var data = {
+    allItems: {
+      exp: [],
+      inc: [],
+    },
+    totals: {
+      exp: 0,
+      inc: 0,
+    },
+  };
+
+  return {
+    addItem: function (type, des, val) {
+      var newItem, ID;
+
+      //[1 2 3 4 5],next ID = 6
+      //[1 2 4 6 8],next ID = 9
+      //ID = last ID + 1
+
+      //create new ID
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      //create new item based on 'inc' or 'exp' type
+      if (type === 'exp') {
+        newItem = new Expense(ID, des, val);
+      } else if (type === 'inc') {
+        newItem = new Income(ID, des, val);
+      }
+
+      //push it into our data structure
+      data.allItems[type].push(newItem);
+
+      //return the new element
+      return newItem;
+    },
+
+    testing: function () {
+      console.log(data);
+    },
+  };
+})();
+
+//UI CONTROLLER
+var UIController = (function () {
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn',
+    incomeContainer: 'income__list',
+    expensesContainer: 'expenses__list',
+  };
+
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, //will be either inc or exp
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+
+    addListItem: function (obj, type) {
+      var html, newHtml, element;
+      //create HTML string with placeholder text
+
+      if (type === 'inc') {
+        element = DOMstrings.incomeContainer;
+
+        html =
+          '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      } else if (type === 'exp') {
+        element = DOMstrings.expensesContainer;
+
+        html =
+          '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      }
+
+      //Replace the placeholder text withsome actual data
+
+      newHtml = html.replace('%id%', obj.id);
+      newHtml = newHtml.replace('%description%', obj.description);
+      newHtml = newHtml.replace('%value%', obj.value);
+
+      //Insert the HTML into the Dom
+      document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+    },
+
+    getDOMstrings: function () {
+      return DOMstrings;
+    },
+  };
+})();
+
+//GLOBAL APP CONTROLLER
+var controller = (function (budgetCtrl, UICtrl) {
+  var setupEventListeners = function () {
+    var DOM = UICtrl.getDOMstrings();
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    //Also add an item when an enter was clicked
+    document.addEventListener('keypress', function (event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+
+  var ctrlAddItem = function () {
+    var input, newItem;
+    //1.get the field input data
+    var input = UICtrl.getInput();
+
+    //2.Add the item to the budget controller
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
+    //3.Add the item to the UI
+    UICtrl.addListItem(newItem, input.type);
+
+    //4.Calculate the budget
+    //5.Display the budget on the UI
+  };
+
+  return {
+    init: function () {
+      console.log('application has started');
+      setupEventListeners();
+    },
+  };
+})(budgetController, UIController);
+
+controller.init();
